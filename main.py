29bf4685724b2da98aafc8ced46fcff5d88b5048
Sara Papandrea <papandrea.sara@outlook.com>
2025-10-24 19:48:47 +1100

    removed old main

--- main.py
+++ main.py
@@ -18,42 +18,6 @@ def main():
     global runners
 
     print("Welcome to the 60secondstofinish Fuzzer!")
-
-    ctx = mp.get_context("forkserver")
-
-    # iterate over all binaries in the binary folder
-    for binary in os.listdir(BINARIES_DIR):
-        print(f"Starting fuzzing for {binary}")
-        binary_path = os.path.join(BINARIES_DIR, binary)
-        example_input = os.path.join(INPUTS_DIR, f"{binary}.txt")
-
-        format = format_type(example_input)
-        if format == JSON:
-            # TODO: create a JSON mutator
-            print("JSON")
-        elif format == CSV:
-            # TODO: create a CSV mutator
-            print("CSV")
-        else:
-            print(f"Invalid format, skipping {binary}")
-            continue
-
-        # TODO: once mutators have been implemented, pass it in to this class instantiation
-        new_runner = Runner(binary_path, None, ctx)
-        new_runner.start()
-
-        runners.append(new_runner)
-
-    # stop each runner (finish all processes/threads)
-    for runner in runners:
-        runner.stop()
-
-    print("Finished Fuzzing!")
-
-def main():
-    global runners
-
-    print("Welcome to the 60secondstofinish Fuzzer!")
     try:
         args = parse_arguments()
 
