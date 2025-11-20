464fb159a9becdd208247e6f439054293abd0fbb
root <eugenia.chan1107@gmail.com>
2025-11-20 20:16:56 +0800

    get ready for main merge

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ from mutate.csv_mutator import CSVMutator
 from mutate.xml_mutator import XMLMutator
 from mutate.mutator import GenericMutator
 from mutate.elf_mutator import ELFMutator
+from mutate.jpg_mutator import JPGMutator
 
 from runner import Runner
 from crashes import CrashHandler
@@ -57,6 +58,9 @@ def binary_process(binary_path, input_path, coverage, processes_data, global_sto
     elif input_format == FormatType.ELF:
         print(f"[{binary_name}] Detected ELF format. Using ELFMutator.")
         mutator = ELFMutator(input_path, input_queue, stop_event, binary_name, max_queue_size)
+    elif input_format == FormatType.JPEG:
+        print(f"[{binary_name}] Detected JPEG format. Using JPGMutator.")
+        mutator = JPGMutator(input_path, input_queue, stop_event, binary_name, max_queue_size)
     else:
         # fallback to generic mutator
         print(f"[{binary_name}] Using GenericMutator for format: {input_format.name}")
